71fed22fd4e466554b1969cd1a3a57d6a1a7ff32
Nisha Khan <nisha.f.khan@gmail.com>
2020-07-12 22:09:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,12 +18,12 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase()) {
     return "I can\'t hear you!"
 }
-  else if (string.toUpperCase()) {
-    return "YES INDEED!";
+  if (string ==== string.toUpperCase()) {
+    return "YES INDEED!"
   }
 
-  else if (string === "I love you, Grandma.") {
-    return "I love you, too.";
+ if (string === "I love you, Grandma.") {
+    return "I love you, too."
   }
 
 }
